46d8f72ac7b6a6cfb7ca149fd37ce7a3d0a3b142
Vili-Taneli Salo <43577732+vilisalo@users.noreply.github.com>
2025-04-22 11:04:51 +0200

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,49 @@
+import numpy as np
+from tools import rotation_matrix
+
+
+class Inversion:
+
+    def get_matrix(self):
+        return -np.identity(3)
+
+
+class Rotation:
+    def __init__(self, axis, order=1):
+        self._order = order
+
+        self._axis = np.array(axis)
+
+    def get_matrix(self):
+
+        return rotation_matrix(self._axis, 2*np.pi / self._order)
+
+
+class ImproperRotation:
+    def __init__(self, axis, order=1):
+        self._order = order
+
+        self._axis = np.array(axis)
+
+    def get_matrix(self):
+
+        rot_matrix = rotation_matrix(self._axis, 2*np.pi / self._order)
+
+        uax = np.dot(self._axis, self._axis)
+        refl_matrix = np.identity(3) - 2 * np.outer(self._axis, self._axis) / uax
+
+        return np.dot(rot_matrix, refl_matrix.T)
+
+
+class Reflection:
+    def __init__(self, axis):
+
+        norm = np.linalg.norm(axis)
+        assert abs(norm) > 1e-8
+        self._axis = np.array(axis) / norm   # normalize axis
+
+    def get_matrix(self):
+        uax = np.dot(self._axis, self._axis)
+
+        return np.identity(3) - 2*np.outer(self._axis, self._axis)/uax
+
